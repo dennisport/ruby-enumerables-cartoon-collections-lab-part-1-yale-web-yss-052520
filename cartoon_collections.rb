ecd560ef7e4b58716f8ade2b4fc0834f632d5533
Dennis Portillo <dennis.portillo@yale.edu>
2020-05-21 18:14:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |element, list|
-    puts "#{list +1.String}, #{element}"
+    puts "#{index}, #{element}"
   end
 end
