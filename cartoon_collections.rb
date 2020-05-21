53a3fd1a7366948ae3a387ee9ba305525afa8338
Dennis Portillo <dennis.portillo@yale.edu>
2020-05-21 18:01:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,10 +2,15 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
+  array.each do |greeting|
+  puts "Hello #{greeting}!"
 end
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
+  array.each_with_index do |element, list|
+    puts "#{element}"
+end
 end
